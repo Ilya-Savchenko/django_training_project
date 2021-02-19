1fe4fd1686087350c419b3792869677578173b53
Ilya-Savchenko <ilya07.iv@gmail.com>
2021-02-20 00:29:34 +0300

    Add filter and search fields in admin

--- training_project/app_news/admin.py
+++ training_project/app_news/admin.py
@@ -6,8 +6,11 @@ from .models import NewsModel, CommentModel
 @admin.register(NewsModel)
 class NewsAdmin(admin.ModelAdmin):
 	list_display = ['title', 'created_at', 'is_active']
+	list_filter = ['is_active', 'created_at', 'title']
+	search_fields = ['title']
 
 @admin.register(CommentModel)
 class CommentAdmin(admin.ModelAdmin):
 	list_display = ['user_name', 'news', 'text', 'created_at']
-
+	list_filter = ['user_name', 'news']
+	search_fields = ['user_name', 'news']
