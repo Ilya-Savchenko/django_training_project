a68a13098f48f2e019a5b8e4558d72bc81dce9f1
Ilya-Savchenko <ilya07.iv@gmail.com>
2021-02-21 19:19:03 +0300

    Removed post method in some views

--- training_project/app_news/admin.py
+++ training_project/app_news/admin.py
@@ -3,11 +3,17 @@ from django.contrib import admin
 from .models import NewsModel, CommentModel
 
 
+class CommentInLine(admin.TabularInline):
+	model = CommentModel
+
+
 @admin.register(NewsModel)
 class NewsAdmin(admin.ModelAdmin):
 	list_display = ['title', 'created_at', 'is_active']
 	list_filter = ['is_active', 'created_at', 'title']
 	search_fields = ['title']
+	inlines = [CommentInLine]
+
 
 @admin.register(CommentModel)
 class CommentAdmin(admin.ModelAdmin):
--- training_project/app_news/views.py
+++ training_project/app_news/views.py
@@ -1,12 +1,8 @@
-from datetime import datetime
-
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
 from django.views.generic.edit import FormMixin
 
-from .forms import CreatedNewsForm, EditingNewsForm, AddedCommentForm
+from .forms import AddedCommentForm
 from .models import NewsModel, CommentModel
 
 
@@ -21,27 +17,14 @@ class CreatedNewsFormView(generic.CreateView):
 	model = NewsModel
 	template_name = 'news/news_form.html'
 	fields = ['title', 'content']
-
-	def post(self, request, *args, **kwargs):
-		created_news_form = CreatedNewsForm(request.POST)
-		if created_news_form.is_valid():
-			NewsModel.objects.create(**created_news_form.cleaned_data)
-			return HttpResponseRedirect('/')
-		return render(request, 'news/created_news.html', {'created_news_form': created_news_form})
+	success_url = '/'
 
 
 class EditNewsFormView(generic.UpdateView):
 	model = NewsModel
 	fields = ['title', 'content', 'is_active']
 	template_name = 'news/news_updating_form.html'
-
-	def post(self, request, *args, **kwargs):
-		edit_news_form = EditingNewsForm(request.POST)
-		if edit_news_form.is_valid():
-			NewsModel.objects.filter(id=self.kwargs['pk']).update(editing_at=datetime.utcnow(),
-			                                                   **edit_news_form.cleaned_data)
-			return HttpResponseRedirect('/')
-		return render(request, 'news/news_updating_form.html', context={'edit_news_form': edit_news_form})
+	success_url = '/'
 
 
 class NewsCommentFormView(FormMixin, generic.DetailView):
@@ -57,7 +40,6 @@ class NewsCommentFormView(FormMixin, generic.DetailView):
 		context = super(NewsCommentFormView, self).get_context_data(**kwargs)
 		comments = CommentModel.objects.filter(news_id=self.kwargs['pk']).order_by('-created_at')
 		context['comments'] = comments
-		context['form'] = AddedCommentForm
 		return context
 
 	def post(self, request, *args, **kwargs):
